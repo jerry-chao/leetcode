1e19840a4ee4715f74fc2e86246102fb895dd54a
chao.zhang <462283159@qq.com>
2020-11-17 23:01:30 +0800

    add prefect squares to project

---
+++ leetcode/279.perfect-squares.go
@@ -0,0 +1,41 @@
+package leetcode
+
+/*
+ * @lc app=leetcode id=279 lang=golang
+ *
+ * [279] Perfect Squares
+ *
+ * https://leetcode.com/problems/perfect-squares/description/
+ *
+ * algorithms
+ * Medium (48.06%)
+ * Likes:    3593
+ * Dislikes: 214
+ * Total Accepted:    350.7K
+ * Total Submissions: 727.4K
+ * Testcase Example:  '12'
+ *
+ * Given a positive integer n, find the least number of perfect square numbers
+ * (for example, 1, 4, 9, 16, ...) which sum to n.
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 12
+ * Output: 3
+ * Explanation: 12 = 4 + 4 + 4.
+ *
+ * Example 2:
+ *
+ *
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ */
+
+// @lc code=start
+func numSquares(n int) int {
+	return 0
+}
+
+// @lc code=end
